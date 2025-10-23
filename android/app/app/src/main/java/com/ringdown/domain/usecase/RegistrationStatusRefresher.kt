a8da9459276d9b1c55069497b24215687a48040a
danshapiro <danshapiro@example.com>
2025-10-22 23:25:39 -0700

    remove android\ because we're giving up on this implementation of it :(

--- android/app/app/src/main/java/com/ringdown/domain/usecase/RegistrationStatusRefresher.kt
+++
@@ -1,7 +0,0 @@
-package com.ringdown.domain.usecase
-
-import com.ringdown.domain.model.DeviceRegistration
-
-fun interface RegistrationStatusRefresher {
-    suspend operator fun invoke(): DeviceRegistration
-}
